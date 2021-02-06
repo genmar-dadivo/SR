06d7414f3dc6f44b82e272310a5dde0aee301329
Genmar Dadivo <31000214+genmar-dadivo@users.noreply.github.com>
2021-02-06 08:18:13 +0800

    remove test.php
    remove temp files.

---
+++ content/pages/csi.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../../assets/css/bs/bootstrap.min.css">
+    <link rel="stylesheet" href="../../assets/css/main/main.css">
+
+    <link rel="stylesheet" href="../../assets/css/dt/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="../../assets/css/dt/responsive.dataTables.min.css">
+
+    <link rel="icon" href="../../assets/img/logo/logo.png" type="image/gif" sizes="30x30">
+
+	<title> CSI | Sales Report </title>
+</head>
+<body>
+	<div id="overlay">
+	</div>
+	<div style="height: 100px;"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="card border-0">
+					<nav class="navbar navbar-light bg-light">
+					</nav>
+				  	<div class="card-body">
+				  		<table id="tcsi" class="table table-striped responsive nowrap" width="100%">
+					        <thead>
+					            <tr>
+					            	<th></th>
+									<th>DBNO</th>
+									<th>OT</th>
+									<th>ORDERNO</th>
+									<th>OS</th>
+									<th>ODE</th>
+									<th>OD</th>
+									<th>OATN</th>
+									<th>OPON</th>
+									<th>OCN</th>
+									<th>CBM</th>
+									<th>SD</th>
+									<th>SHIPCODE</th>
+									<th>TERMS</th>
+									<th>SALESMAN</th>
+									<th>MFGNGLOC</th>
+									<th>TSA</th>
+									<th>TOTALCOST</th>
+									<th>INVOICENO</th>
+									<th>INVOICEDATE</th>
+									<th>OECASHKEY</th>
+									<th>UF1</th>
+									<th>UF2</th>
+									<th>UF3</th>
+									<th>UF4</th>
+									<th>UF5</th>
+									<th>ENCDBY</th>
+					            </tr>
+					        </thead>
+					        <tbody>
+					        </tbody>
+					        <tfoot>
+					            <tr>
+					            	<th></th>
+									<th>DBNO</th>
+									<th>OT</th>
+									<th>ORDERNO</th>
+									<th>OS</th>
+									<th>ODE</th>
+									<th>OD</th>
+									<th>OATN</th>
+									<th>OPON</th>
+									<th>OCN</th>
+									<th>CBM</th>
+									<th>SD</th>
+									<th>SHIPCODE</th>
+									<th>TERMS</th>
+									<th>SALESMAN</th>
+									<th>MFGNGLOC</th>
+									<th>TSA</th>
+									<th>TOTALCOST</th>
+									<th>INVOICENO</th>
+									<th>INVOICEDATE</th>
+									<th>OECASHKEY</th>
+									<th>UF1</th>
+									<th>UF2</th>
+									<th>UF3</th>
+									<th>UF4</th>
+									<th>UF5</th>
+									<th>ENCDBY</th>
+					            </tr>
+					        </tfoot>
+					    </table>
+				  	</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <script src="../../assets/js/jquery/jquery-3.3.1.min.js"></script>
+    <script src="../../assets/js/popper/popper.min.js"></script>
+    <script src="../../assets/js/bs/bootstrap.min.js"></script>
+    <script src="../../assets/js/dt/jquery.dataTables.min.js"></script>
+   	<script src="../../assets/js/dt/dataTables.bootstrap4.min.js"></script>
+	<script src="../../assets/js/dt/dataTables.responsive.min.js"></script>
+    <script src="../../assets/js/dt/dataTables.buttons.min.js"></script>   
+    <script src="../../assets/js/dt/buttons.flash.min.js"></script>    
+    <script src="../../assets/js/dt/buttons.html5.min.js"></script>    
+    <script src="../../assets/js/dt/buttons.print.min.js"></script>
+    <script src="../../assets/js/dt/jszip.min.js"></script>    
+    <script src="../../assets/js/dt/pdfmake.min.js"></script>    
+    <script src="../../assets/js/dt/vfs_fonts.js"></script> 
+    <script src="../../assets/js/main/main.js?1"></script>
+</body>
+</html>
